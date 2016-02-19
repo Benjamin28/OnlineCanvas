1f70b622c0f13c280aedda76dad4e880173362bf
Benjamin28 <benjamin.baker@utexas.edu>
2016-02-19 14:20:16 -0600

    Update canvas.js
    made crazy fill patern

--- canvas.js
+++ canvas.js
@@ -1,4 +1,18 @@
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(0,0,ctx.canvas.width, ctx.canvas.height);
+ctx.fillStyle = "#000000";
+//ctx.fillRect(0,0,ctx.canvas.width, ctx.canvas.height/2);
+
+
+ctx.fillStyle = "#000000";
+
+
+for ( i = 1; i < 100 ; i++){
+
+    ctx.fillStyle = "#" + i%10 + i%10 + "0000";
+    
+    ctx.fillRect(    ctx.canvas.width* ((i-1)/100)/2
+		   , ctx.canvas.height* ((i-1)/100)/2
+		   , ctx.canvas.width - ctx.canvas.width * ((i-1)/100)
+		   , (ctx.canvas.height - ctx.canvas.height  * ((i-1)/100)));
+}
